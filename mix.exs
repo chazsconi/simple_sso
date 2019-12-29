1a3a2f092d3550969f36a030bf473a64196468b6
Charles Bernasconi <charles.bernasconi@xing.com>
2019-12-29 09:32:57 +0100

    Relax simple_auth dep version

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule SimpleSSO.MixProject do
     [
       {:oauth2, "~> 0.9"},
       {:phoenix, "~> 1.2"},
-      {:simple_auth, "~> 1.6.1"},
+      {:simple_auth, "~> 1.6"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
